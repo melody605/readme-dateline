2eb2c9a691647df178b7e5ffdd4496293479fe12
melody605 <melodyvalentine9@gmail.com>
2024-10-27 21:36:30 -0400

    README.md

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type: "input",
+    name: "title",
+    message: "What is your project name?",
+    },
+
+    {type: "input",
+        name: "description",
+        message: "Give a brief description of your project. Why did you build it? What problem does it solve?",
+
+    }
+
+
+
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log(data)
-  
-  return `{#data.title}`
+  if (license === "MIT") {
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  { else if license 
   
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) 
+{ if (license === "MIT") {
+
+  return`## `
+  [![License:MIT]
+([![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+)}
+
+
+
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+if (license==="none"){
+  return"";
+
+}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -22,6 +39,29 @@ function generateMarkdown(data) {
 
 
   ${data.description}
+
+  [Title](#title)
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [License]($license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+
+${data.installation}
+
+${data.license}
+
+${data.contributing}
+
+${data.test}
+
+${data.email}
+
+github.com/${data.Github}
+
 `;
 }
 
