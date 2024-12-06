30440ed30235d7b67bd864c90c099be1de62673d
melody605 <melodyvalentine9@gmail.com>
2024-12-06 13:51:29 -0500

    readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === "MIT") {
+  if (license === "MIT License") {
+    badgeLink = `https://img.shields.io/badge/MIT%20License-purple`;
+  } else if (license==="Eclipse Public License 2.0") {
+      badgeLink = 'https://img.shields.io/badge/Eclipse%20Public%20License%202.0-purple';
+  } else if (license==="Mozilla Public License 2.0") {
+        badgeLink = 'https://img.shields.io/badge/Mozilla%20Public%20License%202.0-purple';
+  } else {
+    return '';
+  }
+  return `![License](${badgeLink})`;
+}
+
+
+
   return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
   { else if license 
   
@@ -12,10 +25,20 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) 
 { if (license === "MIT") {
+  licenseLink = `https://opensource.org/licenses/MIT`;
+} else if (license==="Eclipse Public License 2.0") {
+  licenseLink = `https://www.eclipse.org/legal/epl-2.0/`;
+} else if (license==="Mozilla Public License 2.0") {
+  licenseLink = `https://www.mozilla.org/en-US/MPL/2.0/`;
+} else {
+  return ``;
+}
+return `See the [full license](${licenseLink}) for more information.`;
+  
+}
 
-  return`## `
-  [![License:MIT]
-([![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//   [![License:MIT]
+// ([![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 )}
 
@@ -27,18 +50,23 @@ function renderLicenseLink(license)
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-if (license==="none"){
-  return"";
+
+  return(license) ? `License` : '';
 
 }
-}
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown({projectTitle, description, installation, usageInfo, contributionGuidelines, test, license, github, email, attachmentName}) {
+  return `# ${projectTitle}
+${renderLicenseBadge(license)}
+   
+  ##Description
+
+${description}
 
 
-  ${data.description}
+  ## Table of Contents
 
   [Title](#title)
   [Description](#description)
@@ -50,15 +78,27 @@ function generateMarkdown(data) {
   [Questions](#questions)
 
 
-${data.installation}
 
-${data.license}
+  ##Installation
 
-${data.contributing}
+  ${installation}
 
-${data.test}
 
-${data.email}
+  ${license}
+
+  ##Usage Information
+
+  ${usage.Info}
+  
+  #How to Contribute 
+
+  ${contribution.Guidelines}
+
+  ##Tests
+
+  ${test}
+
+
 
 github.com/${data.Github}
 
